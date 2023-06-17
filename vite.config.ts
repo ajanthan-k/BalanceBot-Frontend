dc47fc2eb3523d68bb24e0b885027c44435bd351
sinlessclown278 <ajanthankanagasabapathy@gmail.com>
2023-06-17 19:03:05 +0100

    Host on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/BalanceBot/"
+  base: "/"
   // server: {
   //   host: true, // needed for the Docker Container port mapping to work
   //   strictPort: true,
